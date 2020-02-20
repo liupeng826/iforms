c5b34859c760ff28a16ac3766c0c5dafde6a883c
PengLiu <peng.liu@volvo.com>
2020-02-20 15:06:07 +0800

    swagger2加入Authorization

--- iforms-common/src/main/java/com/microastudio/iforms/common/config/SwaggerConfig.java
+++ iforms-common/src/main/java/com/microastudio/iforms/common/config/SwaggerConfig.java
@@ -8,14 +8,16 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 /**
  * api页面 /swagger-ui.html
@@ -54,7 +56,11 @@ public class SwaggerConfig {
                 .apiInfo(apiInfo())
                 .select()
                 .paths(Predicates.not(PathSelectors.regex("/error.*")))
-                .build();
+                .build()
+//                .globalOperationParameters(pars)
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts())
+                ;
     }
 
     private ApiInfo apiInfo() {
@@ -62,5 +68,27 @@ public class SwaggerConfig {
                 .title("iForms Interface Document")
                 .version("2.9.2")
                 .build();
+    }
+
+    private List<ApiKey> securitySchemes() {
+        return newArrayList(
+                new ApiKey("Authorization", "Authorization", "header"));
+    }
+
+    private List<SecurityContext> securityContexts() {
+        return newArrayList(
+                SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+                        .forPaths(PathSelectors.regex("^(?!auth).*$"))
+                        .build()
+        );
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return newArrayList(
+                new SecurityReference("Authorization", authorizationScopes));
     }
 }
